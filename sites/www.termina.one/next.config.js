65560a138d61583f13e319ad4e679c79f82da3f4
zuedev <zuedev@gmail.com>
2024-06-04 16:21:10 +0100

    setup static exports

--- sites/www.termina.one/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-// build as standalone if docker is building
-if (process.env.DOCKER_BUILD) nextConfig.output = "standalone";
-
-module.exports = nextConfig;
