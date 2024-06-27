ea3f3c0924f2a2b1dbdd2793b33323f392355d66
Alex <zuedev@gmail.com>
2024-06-27 13:17:23 +0100

    legal update + cf specials

---
+++ src/app/raw/layout.js
@@ -0,0 +1,12 @@
+import "./raw.css";
+
+export default ({ children }) => {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+};
---
+++ src/app/raw/privacy-policy/page.js
@@ -0,0 +1,11 @@
+export default () => {
+  return (
+    <>
+      <h1>Privacy Policy</h1>
+      <p>
+        Our privacy policy is simple: we don't collect any personal information
+        about you.
+      </p>
+    </>
+  );
+};
