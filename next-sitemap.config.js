528ee2b36ff366e2b0f72a8e8b8e1307f0067aed
zuedev <zuedev@gmail.com>
2024-08-23 13:31:28 +0100

    new site (needs framework)

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: "https://termina.one",
-  outDir: "out",
-  generateRobotsTxt: true,
-  generateIndexSitemap: false,
-};
--- src/components/TopBar/index.js
+++
@@ -1,9 +0,0 @@
-export default (props) => {
-  let { className, children } = props;
-
-  if (!children) throw new Error("TopBar requires children");
-
-  if (!className) className = "bg-[yellow] text-black text-center px-2 py-1";
-
-  return <div className={className}>{children}</div>;
-};
