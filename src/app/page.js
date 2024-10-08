dc16808531abaa81fb051fa09e49d64d3f9e0c72
zuedev <zuedev@gmail.com>
2024-10-08 21:25:54 +0100

    better dist of home items

--- src/app/page.js
+++ src/app/page.js
@@ -16,45 +16,54 @@ export default () => {
         </h2>
       </section>
 
-      <section className="py-16 space-y-4">
-        <div className="max-w-prose mx-auto">
-          <h3 className="text-4xl font-bold">What makes us different?</h3>
-        </div>
-        <div className="container mx-auto">
-          <div className="space-y-2 max-w-prose mx-auto">
-            {[
-              {
-                title: "✨ Bleeding-edge technology",
-                description:
-                  "We are always on the lookout for the latest technologies to help our clients stay ahead of the competition.",
-              },
-              {
-                title: "🌍 Planet-wide reach",
-                description:
-                  "Our network of data centers and edge locations span the globe, ensuring that your data is always close to your customers.",
-              },
-              {
-                title: "⚡️ Lightning-fast performance",
-                description:
-                  "No corners are cut when it comes to the performance of our hardware. We only use the best-in-class components to ensure that your applications run smoothly.",
-              },
-              {
-                title: "🔒 Secure by design",
-                description:
-                  "Security is our top priority. We use the latest encryption technologies to ensure that your data is always safe.",
-              },
-              {
-                title: "🧰 Vast toolkit",
-                description:
-                  "Our team of experts has a wide range of skills and tools at their disposal to help you build the perfect solution for your business.",
-              },
-            ].map((item, index) => (
-              <p key={index}>
-                <b>{item.title}: </b>
-                {item.description}
-              </p>
-            ))}
-          </div>
+      <section className="container mx-auto py-16">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          {[
+            {
+              emoji: "✨",
+              title: "Bleeding-edge technology",
+              description:
+                "We are always on the lookout for the latest technologies to help our clients stay ahead of the competition.",
+            },
+            {
+              emoji: "🌍",
+              title: "Planet-wide reach",
+              description:
+                "Our network of data centers and edge locations span the globe, ensuring that your data is always close to your customers.",
+            },
+            {
+              emoji: "⚡️",
+              title: "Lightning-fast performance",
+              description:
+                "No corners are cut when it comes to the performance of our hardware. We only use the best-in-class components to ensure that your applications run smoothly.",
+            },
+            {
+              emoji: "🔒",
+              title: "Secure by design",
+              description:
+                "Security is our top priority. We use the latest encryption technologies to ensure that your data is always safe.",
+            },
+            {
+              emoji: "🧰",
+              title: "Vast toolkit",
+              description:
+                "Our team of experts has a wide range of skills and tools at their disposal to help you build the perfect solution for your business.",
+            },
+            {
+              emoji: "🏢",
+              title: "Enterprise-grade solutions",
+              description:
+                "We have worked with some of the largest companies in the world to deliver enterprise-grade solutions that are scalable and reliable.",
+            },
+          ].map((item, index) => (
+            <div key={index} className="p-4 flex items-center space-x-4">
+              <div className="text-4xl">{item.emoji}</div>
+              <div>
+                <h3 className="font-bold text-xl">{item.title}</h3>
+                <p className="text-justify">{item.description}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </section>
     </div>
