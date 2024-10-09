71e5cbbbea57b2f2b115ab724758ef956e363dda
zuedev <zuedev@gmail.com>
2024-10-09 14:07:23 +0100

    better nav elements

--- src/components/MainNav/index.js
+++ src/components/MainNav/index.js
@@ -1,3 +1,7 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
 export default () => {
   const links = [
     {
@@ -35,15 +39,26 @@ export default () => {
     },
   ];
 
+  const pathname = usePathname();
+
+  for (const link of links) {
+    link.active = link.url === pathname;
+
+    link.className = ["hover:underline", "hover:decoration-dotted"];
+
+    if (link.active) link.className.push("underline");
+
+    if (link.disabled)
+      link.className.push("opacity-50 line-through cursor-not-allowed");
+  }
+
   return (
     <nav className="flex flex-col md:flex-row md:space-x-4 font-bold">
-      {links.map(({ name, url, disabled }) => (
+      {links.map(({ name, url, disabled, className }) => (
         <a
           key={name}
           href={disabled ? "#" : url}
-          className={
-            disabled ? "opacity-50 line-through cursor-not-allowed" : ""
-          }
+          className={className.join(" ")}
           title={disabled ? "Coming soon..." : ""}
         >
           {name}
