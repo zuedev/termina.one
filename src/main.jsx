801ea157ed40370dcf0271a293ceff61f63aeeb5
Alex Pooley <zuedev@gmail.com>
2025-02-05 19:07:19 +0000

    feat: implement routing with react-router and restructure main component

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import t1Logo from "/t1_logo_tight.png";
-
-function App() {
-  return (
-    <div className="bg-white text-black dark:bg-black dark:text-white min-h-screen">
-      <div className="p-8 space-y-8 max-w-2xl text-justify font-mono">
-        <img src={t1Logo} alt="Termina One Logo" className="h-10" />
-        <p className="font-bold text-2xl">
-          Termina One creates, builds, and implements cutting-edge cloud
-          infrastructure solutions.
-        </p>
-        <p className="text-lg">
-          Our software and services deliver lightning-fast hosting to millions
-          worldwide, ensuring platforms rely on Termina One for unmatched
-          performance, reliability, and scalability.
-        </p>
-      </div>
-    </div>
-  );
-}
+import { BrowserRouter, Routes, Route } from "react-router";
+import "./global.css";
+import IndexPage from "./index.jsx";
+import StatusPage from "./status.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<IndexPage />} />
+        <Route path="/status" element={<StatusPage />} />
+        <Route path="*" element={<IndexPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
---
+++ src/status.jsx
@@ -0,0 +1,7 @@
+export default () => {
+  return (
+    <div className="bg-white text-black dark:bg-black dark:text-white min-h-screen">
+      owo
+    </div>
+  );
+};
